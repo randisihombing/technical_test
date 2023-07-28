86a3d54d6a67dc86ca0cd997b0dcbccd03402282
randi.lumbantoruan <randi.lumbantoruan@primecash.co.id>
2023-07-28 22:27:36 +0700

    second commit

--- lib/product/component/more_menu.dart
+++ lib/product/component/more_menu.dart
@@ -38,13 +38,12 @@ class _MoreMenuState extends State<MoreMenu> {
                 ),
               ),
               child: Padding(
-                padding: const EdgeInsets.all(10),
+                padding: const EdgeInsets.only(left: 20, top: 10, right: 20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Text(
                       'Features',
-
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20
@@ -60,35 +59,169 @@ class _MoreMenuState extends State<MoreMenu> {
                         children: <Widget>[
                           Container(
                             padding: const EdgeInsets.all(8),
-                            color: Colors.teal[100],
-                            child: const Text("He'd have you all unravel at the"),
+                            color: Colors.grey,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                    'assets/icons/instagram_live.png',
+                                  height: 40,
+                                  width: 40,
+                                ),
+                                const Text(
+                                    "Live Show",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                           Container(
                             padding: const EdgeInsets.all(8),
-                            color: Colors.teal[200],
-                            child: const Text('Heed not the rabble'),
+                            color: Colors.black12,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                    'assets/icons/homework.png',
+                                  height: 40,
+                                  width: 40,
+                                ),
+                                const Text(
+                                    "E-Course",
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                           Container(
                             padding: const EdgeInsets.all(8),
-                            color: Colors.teal[300],
-                            child: const Text('Sound of screams but the'),
+                            color: Colors.black26,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                    'assets/icons/community.png',
+                                  height: 40,
+                                  width: 40,
+                                ),
+                                const Text(
+                                    "Community",
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                           Container(
                             padding: const EdgeInsets.all(8),
-                            color: Colors.teal[400],
-                            child: const Text('Who scream'),
+                            color: Colors.black38,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                    'assets/icons/user.png',
+                                  height: 40,
+                                  width: 40,
+                                ),
+                                const Text(
+                                    "My Profile",
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                           Container(
                             padding: const EdgeInsets.all(8),
-                            color: Colors.teal[500],
-                            child: const Text('Revolution is coming...'),
+                            color: Colors.black45,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                    'assets/icons/recent.png',
+                                  height: 40,
+                                  width: 40,
+                                ),
+                                const Text(
+                                    "Saved Course",
+                                  style: TextStyle(
+                                      fontSize: 8,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                           Container(
                             padding: const EdgeInsets.all(8),
-                            color: Colors.teal[600],
-                            child: const Text('Revolution, they...'),
+                            color: Colors.black54,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                    "assets/icons/clipboard.png",
+                                  height: 40,
+                                  width: 40,
+                                ),
+                                const Text(
+                                    "My List",
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
-                        ],
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            color: Colors.black45,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                    'assets/icons/shopping_cart.png',
+                                  height: 40,
+                                  width: 40,
+                                ),
+                                const Text(
+                                    "My Cart",
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            color: Colors.black12,
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                    'assets/icons/clipboard.png',
+                                  height: 40,
+                                  width: 40,
+                                ),
+                                const Expanded(
+                                  child: Text(
+                                      "Purchase History",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 7,
+                                        fontWeight: FontWeight.bold
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ]
                       ),
                     ),
                   ],
